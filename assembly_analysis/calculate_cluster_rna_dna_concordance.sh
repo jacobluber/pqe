5bd74c555e92f38a4dd607c0da8519785835cade
Jacob Luber <jluber@g.harvard.edu>
2018-04-22 14:52:59 +0000

    calculate cluster dna/rna concordance

---
+++ assembly_analysis/calculate_cluster_rna_dna_concordance.sh
@@ -0,0 +1,9 @@
+#!/bin/bash,
+echo "dna_only,all_rna_concordant,at_least_one_matching,at_least_one_concordant,total_families_with_dna,all_concordant,all_dna_concordant,total_families_with_rna" > rna_dna_stability.txt
+for indiv in $(cat assembles); do
+	rm -rf all.clstr.mod
+	aws s3 cp s3://jacob-pqe/assemblies/${indiv}/genecatalogs/all.clstr.mod all.clstr.mod 
+	python modclstr.py all.clstr.mod | tr '\n' ' ' < <(python modclstr.py all.clstr.mod | sed 's/\t/,/g' | cut -d, -f2 ) >> rna_dna_stability.txt;
+done
+rm -rf all.clstr.mod
+	
